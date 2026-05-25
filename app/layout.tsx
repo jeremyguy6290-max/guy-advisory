a18dbeda180ad84ab193f8efe25d017cfd75ca26
jeremyguy6290-max <jeremyguy6290@gmail.com>
2026-05-26 07:13:35 +1200

    Move GA4 scripts to head for Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col font-[var(--font-inter)]">
+      <head>
         {gaId && (
           <>
             <Script
@@ -40,6 +40,8 @@ export default function RootLayout({
             </Script>
           </>
         )}
+      </head>
+      <body className="min-h-full flex flex-col font-[var(--font-inter)]">
         {children}
       </body>
     </html>
